ccd3ba9e8efebae573b6cf9ab08c5276f1dae8fd
AkankshaAB123 <akankshaashok2895@gmail.com>
2026-05-15 22:43:01 +0530

    fixed render deployment

--- server/server.js
+++ server/server.js
@@ -6,14 +6,16 @@ const cors = require("cors");
 
 const studyRoutes = require("./routes/studyRoutes");
 const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 app.use("/study", studyRoutes);
 app.use("/auth", authRoutes);
@@ -22,6 +24,8 @@ app.get("/", (req, res) => {
   res.send("Server Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
